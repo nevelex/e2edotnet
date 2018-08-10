4a73770261e64d1613a2112e0b2ab69014fcfacf
Brian Bosak <bbosak@nevelex.com>
2018-08-10 13:25:34 -0500

    Addressed review comments.

--- E2EDotNet.Tests/Properties/AssemblyInfo.cs
+++ E2EDotNet.Tests/Properties/AssemblyInfo.cs
@@ -1,4 +1,25 @@
-﻿// REVIEW(pstein): Add License header here
+﻿/*
+Copyright (c) 2018 Nevelex Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+// REVIEW(pstein): Add License header here
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -10,10 +31,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 // REVIEW(pstein): Add Nevelex Corporation here
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Nevelex Corporation")]
 [assembly: AssemblyProduct("E2EDotNet.Tests")]
 // REVIEW(pstein): Add Nevelex Corporation here
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Nevelex Corporation 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
